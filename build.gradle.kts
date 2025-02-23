f573dd3bb58ca946efae989127540177e46f1c4c
syncended <syncended@gmail.com>
2025-02-24 02:56:55 +0300

    add publish task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
+import org.jreleaser.model.Active
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.jreleaser)
+    `maven-publish`
 }
-
-group = "dev.syncended.whois"
-version = "0.1"
 
 repositories {
     mavenCentral()
@@ -11,6 +12,7 @@ repositories {
 
 dependencies {
     implementation(libs.commons.net)
+
     testImplementation(libs.junit.engine)
     testImplementation(libs.junit.params)
     testImplementation(libs.kotest.assertions)
@@ -20,6 +22,137 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+
+tasks["publish"].doFirst {
+    File(layout.buildDirectory.get().asFile, "jreleaser").mkdirs()
+}
+
+publishing {
+    publications {
+        repositories {
+            maven { url = stagingDir().toURI() }
+        }
+        create<MavenPublication>("mavenPublications") {
+            from(components["java"])
+            groupId = "dev.syncended"
+            artifactId = "whois"
+            version = releaseVersion()
+
+            pom {
+                name = "whois"
+                description = "Whois Library"
+                url = "https://github.com/syncended/whois"
+                scm {
+                    connection = "scm:git:https://github.com/syncended/whois"
+                    developerConnection = "scm:git:https://github.com/syncended/"
+                    url = "https://github.com/syncended/whois"
+                }
+
+                licenses {
+                    license {
+                        name = "Apache-2.0 license"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "syncended"
+                        name = "Mikhail Ivanov"
+                        email = "syncended@gmail.com"
+                    }
+                }
+            }
+        }
+    }
+}
+
+jreleaser {
+    dryrun = false
+    gitRootSearch = true
+    version = releaseVersion()
+
+    signing {
+        active = Active.ALWAYS
+        armored = true
+        verify = true
+        artifacts = true
+        files = true
+
+        publicKey = gpgPublicKey()
+        secretKey = gpgKey()
+        passphrase = gpgPassword()
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                active = Active.ALWAYS
+                create("sonatype") {
+                    active = Active.ALWAYS
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    println("Sign dir: ${stagingDir()}")
+                    stagingRepository(stagingDir().toString())
+
+                    username = mavenUsername()
+                    password = mavenPassword()
+                }
+            }
+        }
+    }
+    release {
+        github {
+            skipRelease = true
+            skipTag = true
+            overwrite = false
+            token = "none"
+        }
+    }
+}
+
+
+private fun stagingDir(): File {
+    return layout.buildDirectory.dir("staging").get().asFile
+}
+
+private fun mavenRepository(): String {
+    val hasReleaseVer = System.getenv("RELEASE_VERSION") != null
+    return if (hasReleaseVer) {
+        "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    } else {
+        "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    }
+}
+
+private fun releaseVersion(): String {
+    return System.getenv("RELEASE_VERSION")
+        ?: "${System.currentTimeMillis()}-SNAPSHOT"
+}
+
+private fun mavenUsername(): String {
+    return System.getenv("SONATYPE_USERNAME") ?: ""
+}
+
+private fun mavenPassword(): String {
+    return System.getenv("SONATYPE_PASSWORD") ?: ""
+}
+
+private fun gpgPublicKey(): String? {
+    return System.getenv("GPG_PUBLIC_KEY")
+}
+
+private fun gpgKey(): String? {
+    return System.getenv("GPG_KEY")
+}
+
+private fun gpgPassword(): String? {
+    return System.getenv("GPG_PASSWORD")
 }
